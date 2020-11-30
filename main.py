54cc4e8e853f716b665156dcbb5ea1ce443f67e7
Agustin <agustin@craftech.io>
2020-11-30 11:36:34 -0300

    Fix PEP8 warnings

--- main.py
+++ main.py
@@ -5,22 +5,26 @@ import json
 from string import Template
 
 parser = argparse.ArgumentParser(description='Convert environment files to kubernetes secrets')
-parser.add_argument('--name', metavar='name', nargs='?', type=str, default='my-secrets', help='Name of the secret store')
-parser.add_argument('--env', metavar='.env', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Environment input file, stdin by default')
-parser.add_argument('--secrets', metavar='.yaml', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='Secrets output file, stdout by default')
+parser.add_argument('--name', metavar='name', nargs='?', type=str, default='my-secrets',
+                    help='Name of the secret store')
+parser.add_argument('--env', metavar='.env', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
+                    help='Environment input file, stdin by default')
+parser.add_argument('--secrets', metavar='.yaml', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
+                    help='Secrets output file, stdout by default')
 
 
-def loadFiles(secret):
-    if (secret[1].startswith('filecontent=')):
+def load_files(secret):
+    if secret[1].startswith('filecontent='):
         with open(secret[1][12:], 'r') as secretfile:
             data = secretfile.read()
         return [secret[0], data]
     return secret
 
-def yaml_ouput(encodedSecrets):
-    '''secrets its a tuple with key, value format'''
 
-    yamlTemplate = Template("""
+def yaml_ouput(encoded_secrets):
+    """secrets its a tuple with key, value format"""
+
+    yaml_template = Template("""
 apiVersion: v1
 kind: Secret
 metadata:
@@ -28,35 +32,38 @@ metadata:
 type: Opaque
 data:
 $encodedSecrets""")
-    yamlOutput = yamlTemplate.substitute(name=args.name, encodedSecrets='\n'.join(encodedSecrets))
+    yaml_output = yaml_template.substitute(name=args.name, encodedSecrets='\n'.join(encoded_secrets))
 
-    args.secrets.write(yamlOutput)
+    args.secrets.write(yaml_output)
     args.secrets.close()
 
-def process_plainfile(args):
-    config_lines = args.env.readlines()
-    args.env.close()
-    secrets = [line.split('=', 1) for line in config_lines]
-    secrets = map(loadFiles, secrets)
 
-    encodedSecrets = ['  {0}: {1}'.format(
+def process_plainfile(_args):
+    config_lines = _args.env.readlines()
+    _args.env.close()
+    secrets = [line.split('=', 1) for line in config_lines]
+    secrets = map(load_files, secrets)
+
+    encoded_secrets = ['  {0}: {1}'.format(
         secret[0],
         base64.b64encode(secret[1].replace('\n', '').encode('utf-8')).decode('utf-8')
     ) for secret in secrets]
 
-    yaml_ouput(encodedSecrets)
+    yaml_ouput(encoded_secrets)
 
-def process_json(args):
-    json_dict = json.loads(args.env.read())
-    args.env.close()
+
+def process_json(_args):
+    json_dict = json.loads(_args.env.read())
+    _args.env.close()
     secrets = list(json_dict.items())
-    encodedSecrets = [
+    encoded_secrets = [
         '  {0}: {1}'.format(
-                secret[0],
-                base64.b64encode(str(secret[1]).encode('utf-8')).decode('utf-8')
+            secret[0],
+            base64.b64encode(str(secret[1]).encode('utf-8')).decode('utf-8')
         )
-    for secret in secrets]
-    yaml_ouput(encodedSecrets)
+        for secret in secrets]
+    yaml_ouput(encoded_secrets)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
